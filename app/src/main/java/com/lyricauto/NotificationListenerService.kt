71c3032223b25f4c6a3171cb74454ec60d07f1dc
HMwancheng <HM_wancheng@outlook.com>
2025-12-30 00:57:34 +0000

    Fix NotificationListenerService implementation

--- app/src/main/java/com/lyricauto/NotificationListenerService.kt
+++ app/src/main/java/com/lyricauto/NotificationListenerService.kt
@@ -1,6 +1,15 @@
 package com.lyricauto
 
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
 
 class NotificationListenerService : NotificationListenerService() {
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+    }
 }
